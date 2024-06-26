0e5ea5e6341c6e253f99f079b4c2006ef2a22274
7yuningwz <william.zhang7077@gmail.com>
2024-06-25 22:41:37 -0400

    deleted serverURL

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -137,7 +137,7 @@ export default hopeTheme({
     // You MUST generate and use your own comment service in production.
     comment: {
       provider: "Waline",
-      serverURL: "https://comment.sevenning.com/ui/register",
+      serverURL: "https://comment.sevenning.com",
     },
 
     // components:{
