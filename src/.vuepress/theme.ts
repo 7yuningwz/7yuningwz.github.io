681571e2f36aad55f74f979cf3219cc50b77d29d
7yuningwz <william.zhang7077@gmail.com>
2024-06-27 21:47:09 -0400

    fixed true and false

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -172,8 +172,8 @@ export default hopeTheme({
     // These features are enabled for demo, only preserve features you need here
     mdEnhance: {
       tabs: true,
-      chart: true,
-      echarts: true,
+      chart: false,
+      echarts: false,
       mermaid: true,
       sup: true,
       katex: true,
@@ -183,10 +183,10 @@ export default hopeTheme({
       revealJs: false,
       footnote: true,
       hint: true,
-      flowchart: true,
+      flowchart: false,
       gfm: true,
       align: true,
-      attrs: true,
+      attrs: false,
       codetabs: true,
       component: true,
       demo: true,
@@ -194,7 +194,7 @@ export default hopeTheme({
       imgLazyload: true,
       imgSize: true,
       include: true,
-      mark: true,
+      mark: false,
       plantuml: true,
       spoiler: true,
       stylize: [
@@ -213,100 +213,9 @@ export default hopeTheme({
       sub: true,
       vPre: true,
 
-      // install chart.js before enabling it
-      // chart: true,
-
-      // insert component easily
-
-      // install echarts before enabling it
-      // echarts: true,
-
-      // install flowchart.ts before enabling it
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // install katex before enabling it
-      // katex: true,
-
-      // install mathjax-full before enabling it
-      // mathjax: true,
-
-      // install mermaid before enabling it
-      // mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // install @vue/repl before enabling it
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
+      
     },
 
-    // install @vuepress/plugin-pwa and uncomment these if you want a PWA
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cacheImage: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
+    
   },
 });
