6d36c2acd9ca05d378e238e56c6847f620d5c2cd
7yuningwz <william.zhang7077@gmail.com>
2024-06-27 22:03:44 -0400

    add 图标

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,5 +2,45 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   "/",
+  {
+   text: "Blogs",
+   icon: "",
+   link:"#"
+  },
+  {
+    text: "Memories",
+    icon: "",
+    link:"#"
+   },
+   {
+    text: "Communities",
+    icon: "",
+    link:"#"
+   },
+   {
+    text: "Friends",
+    icon: "",
+    link:"#"
+   },
+   {
+    text: "Comments",
+    icon: "",
+    link:"#"
+   },
+   {
+    text: "Products",
+    icon: "",
+    link:"#"
+   },
+   {
+    text: "Tools",
+    icon: "",
+    link:"#"
+   },
+   {
+    text: "Help",
+    icon: "",
+    link:"#"
+   },
   
 ]);
